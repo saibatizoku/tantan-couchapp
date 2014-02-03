d9f5dec0698c20630f817720aaacdc2876befeb4
Joaquin R <globojorro@gmail.com>
2014-02-02 22:38:06 -0600

    Vistas de la barra de navegación

---
+++ _attachments/js/TanTan.Nav.js
@@ -0,0 +1,111 @@
+/*global TodoMVC */
+'use strict';
+
+TanTan.module('Vistas', function (Vistas, App, Backbone, Marionette, $, _) {
+
+    Vistas.NavBar = Marionette.Layout.extend({
+        template: '#template-navbar',
+        className: 'container-fluid',
+        model: App.Docs.UserDoc,
+        regions: {
+            menu: '#nav-menu',
+            actions: '#nav-actions'
+        },
+        userName: function () {
+            if ((this.model) && (this.model.get('name')) && (this.model.get('name').length > 0)) {
+                return this.model.get('name');
+            }
+        },
+        modelEvents: {
+            "change:name": "nameChanged",
+            "error": "resetUser",
+            "sync": "setUser"
+        },
+        resetUser: function () {
+            this.model.reset();
+            this.render();
+        },
+        setUser: function (model, resp, options) {
+            console.log('username synced', resp);
+            this.model = model;
+            this.render();
+        },
+        nameChanged: function (model, value, options) {
+            console.log('username changed', value);
+            model.setIdFromName(value);
+            model.fetch();
+        },
+        onRender: function () {
+            console.log('navbar model', this.model);
+            if (this.userName()) {
+                this.showLoggedIn(this.model);
+            } else {
+                this.showLoggedOut();
+            }
+        },
+        showLoggedIn: function (user) {
+            var usr = user || this.model;
+            this.menu.show(new Vistas.NavMenu({model: usr}));
+            var actions = new Vistas.LoggedIn({model: usr});
+            this.listenTo(actions, "app:logout", function (args) {
+                this.trigger('do:logout', args);
+            });
+            this.actions.show(actions);
+        },
+        showLoggedOut: function () {
+            this.menu.close();
+            var actions = new Vistas.LoggedOut();
+            this.listenTo(actions, "app:login", function (args) {
+                this.trigger('do:login', args);
+            });
+            this.actions.show(actions);
+        }
+    });
+
+    Vistas.NavMenu = Marionette.ItemView.extend({
+        tagName: "ul",
+        className: "nav navbar-nav",
+        getTemplate: function () {
+            if (this.model.is_admin()) {
+                return "#template-nav-menu-admin";
+            } else {
+                return "#template-nav-menu";
+            }
+        }
+    });
+
+    Vistas.LoggedIn = Marionette.ItemView.extend({
+        tagName: "form",
+        className: "navbar-form navbar-right",
+        getTemplate: function () {
+            if (this.model.is_admin()) {
+                return "#template-nav-logged-in-admin";
+            } else {
+                return "#template-nav-logged-in";
+            }
+        },
+        ui: {
+            nuevagranja: ".boton-agregar",
+            submit: "button[type=submit]"
+        },
+        triggers: {
+            "click @ui.nuevagranja": "create:granja",
+            "click @ui.submit": "app:logout"
+        }
+    });
+
+    Vistas.LoggedOut = Marionette.ItemView.extend({
+        template: "#template-nav-logged-out",
+        tagName: "form",
+        className: "navbar-form navbar-right",
+        ui: {
+            user: "input[type=text]",
+            pwd: "input[type=password]",
+            submit: "button[type=submit]"
+        },
+        triggers: {
+            "click @ui.submit": "app:login"
+        }
+    });
+
+});
